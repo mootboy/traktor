e72eed2df5fde1c458610ae67bd84e06a6d6dea2
Linus Harling <linus.harling@gmail.com>
2016-07-21 22:00:16 +0200

    "Working" servo. Lots of stutter.
    It seems I cannot get a clean pulse, broadly speaking duty cycles
    of divisors of 3 works well, i.e. 33333, 20000, 23333 etc. Others
    cause the servo to jitter back and forth.
    
    The Arduino reports nice pulse values for the working dutys, but
    values between 2 and 9 for the "bad" dutys.
    
    Next step is writing my own servo control. The plan is to let the
    timer trigger on the correct pulse length and keep the rest of the
    period low. I may need a "next" buffer to keep the incoming change
    and insert it in the following period instead of inserting it into
    the current.

--- traktor.c
+++ traktor.c
@@ -6,25 +6,64 @@
 #include "os_type.h"
 #include "user_interface.h"
 #include "espconn.h"
+#include "pwm.h"
+
+#define SERVO_CNT_DUTY_US 1500 // 1.5 milliseconds as per spec
+// according to http://www.servodatabase.com/servo/hitec/hs-50
+#define SERVO_MIN_DUTY_US 600
+#define SERVO_MAX_DUTY_US 2400
+
+#define SERVO_CNT_DUTY ((SERVO_CNT_DUTY_US * 1000) / 45)
+#define SERVO_MIN_DUTY ((SERVO_MIN_DUTY_US * 1000) / 45)
+#define SERVO_MAX_DUTY ((SERVO_MAX_DUTY_US * 1000) / 45)
+
+#define SERVO_SLOPE (SERVO_MAX_DUTY - SERVO_MIN_DUTY) / 179
+
+#define PWM_0_OUT_IO_MUX  PERIPHS_IO_MUX_GPIO2_U
+#define PWM_0_OUT_IO_NUM  2
+#define PWM_0_OUT_IO_FUNC FUNC_GPIO2
+#define PWM_CHANNEL 1
 
 struct espconn udp_conn;
 esp_udp udp;
+uint32 duty[PWM_CHANNEL] = {SERVO_CNT_DUTY};
 
-LOCAL void servo_set_deg(short degrees)
+LOCAL void servo_set_deg(uint32 degrees)
 {
-	os_printf("degrees: %d\n", degrees);
+	uint32 dt;
+	if (degrees < 361 && degrees >= 0) {
+		dt = SERVO_MIN_DUTY + SERVO_SLOPE * degrees;
+	} else {
+		dt = degrees;
+	}
+	pwm_set_duty(dt, 0);
+	duty[0] = pwm_get_duty(0);
+	os_printf("duty: %d\n", duty[0]);
 }
 
 LOCAL void ICACHE_FLASH_ATTR
 servo_conn_recv_cb(void *arg, char *data, unsigned short len)
 {
-	if (len < 4)
+	if (len < 32)
 	{
 		char *end;
-		servo_set_deg((short)strtol(data, &end, 10));
+		servo_set_deg((uint32)strtol(data, &end, 10));
 	} else {
 		os_printf("too long data: %s", data);
 	}
+}
+
+void setup_pwm()
+{
+	//PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+	//gpio_output_set(0,0,(1 << 2), 0);
+	uint32 io_info[][3] = {{PWM_0_OUT_IO_MUX, PWM_0_OUT_IO_FUNC, PWM_0_OUT_IO_NUM}};
+	//uint32 period = 21600;
+	uint32 period = 1000000;
+	pwm_init(period, duty, PWM_CHANNEL, io_info);
+	pwm_start();
+	os_printf("pwm initialized, period: %d\n", pwm_get_period());
+	os_printf("duty 0: %d\n", pwm_get_duty(0));
 }
 
 void ICACHE_FLASH_ATTR user_set_station_config(void)
@@ -66,6 +105,7 @@ wifi_event_handler(System_Event_t *event)
 			os_printf("udp set up, registering callback\n");
 			espconn_regist_recvcb(&udp_conn, servo_conn_recv_cb);
 			os_printf("registered callback\n");
+			setup_pwm();
 			break;
 		default: break;
 	}
